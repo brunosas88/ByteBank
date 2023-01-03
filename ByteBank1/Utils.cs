e5ec84faaaa76f5b0b57961d07121d29e2f051bd
brunosas88 <bruno.sas@live.com>
2023-01-03 16:52:46 -0300

    feat: add i/o operations for bank transactions
    Added data persistence and creation
    of text files for each performed transaction

--- ByteBank1/Utils.cs
+++ ByteBank1/Utils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Reflection.Emit;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -11,7 +12,7 @@ namespace ByteBank1
 {
 	public class Utils
 	{
-		public static void WriteJSON(List<Client> clients)
+		public static void WriteJSON(List<Client> clients, List<BankTransactionRecord> bankTransactions)
 		{
 			string filePath = @"C:\Users\Public\Documents\clients.json";
 
@@ -20,9 +21,16 @@ namespace ByteBank1
 			using (StreamWriter outputFile = new StreamWriter(filePath))
 				outputFile.WriteLine(jsonStringClients);
 
+			filePath = @"C:\Users\Public\Documents\bankTransactions.json";
+
+			string jsonStringBankTransactions = JsonSerializer.Serialize(bankTransactions, new JsonSerializerOptions() { WriteIndented = true });
+
+			using (StreamWriter outputFile = new StreamWriter(filePath))
+				outputFile.WriteLine(jsonStringBankTransactions);
+
 		}
 
-		public static void ReadJSON(ref List<Client> clients)
+		public static void ReadJSON(ref List<Client> clients, ref List<BankTransactionRecord> bankTransactions)
 		{
 			string filePath = @"C:\Users\Public\Documents\clients.json";
 
@@ -34,6 +42,58 @@ namespace ByteBank1
 					clients = JsonSerializer.Deserialize<List<Client>>(json);
 				}
 			}
+
+			filePath = @"C:\Users\Public\Documents\bankTransactions.json";
+
+			if (File.Exists(filePath))
+			{
+				using (StreamReader inputFile = new StreamReader(filePath))
+				{
+					string json = inputFile.ReadToEnd();
+					bankTransactions = JsonSerializer.Deserialize<List<BankTransactionRecord>>(json);
+				}
+			}
+		}
+
+		public static void WriteBankTransactionRecordFile (BankTransactionRecord record)
+		{
+			string directoryPath = @"C:\Users\Public\Documents\BankTransactionRecords";
+
+			Directory.CreateDirectory(directoryPath);
+
+			string filePath = @$"{directoryPath}\{record.Id}.txt";
+
+			using StreamWriter file = File.CreateText(filePath);
+
+			file.WriteLine(Display.alignMessage(new string('=', 82)));
+			file.WriteLine(Display.alignMessage($"REDE DE AGENCIAS BYTE BANK"));
+			file.WriteLine(Display.alignMessage($"COMPROVANTE DE {record.OperationType.ToUpper()}"));			
+			file.WriteLine(Display.alignMessage($"{record.Date}"));
+			file.WriteLine(Display.alignMessage(new string('-', 82)));
+			file.WriteLine(Display.alignMessage($"ID: {record.Id}"));
+			file.WriteLine(Display.alignMessage($"VALOR: R$ {record.Value:F2}"));
+			file.WriteLine(Display.alignMessage(new string('-', 82)));
+
+			if (record.OperationType == BankOperation.Deposit.ToString() || record.OperationType == BankOperation.Withdraw.ToString())
+			{				
+				file.WriteLine(Display.alignMessage($"CONTA: {record.OriginClient.AccountNumber}"));
+				file.WriteLine(Display.alignMessage($"NOME: {record.OriginClient.Name}"));
+				file.WriteLine(Display.alignMessage($"CPF: {record.OriginClient.Cpf}"));
+			}
+			else
+			{
+				file.WriteLine(Display.alignMessage("DESTINO"));
+				file.WriteLine(Display.alignMessage($"CONTA: {record.DestinationClient.AccountNumber}"));
+				file.WriteLine(Display.alignMessage($"NOME: {record.DestinationClient.Name}"));
+				file.WriteLine(Display.alignMessage($"CPF: {record.DestinationClient.Cpf}"));
+				file.WriteLine(Display.alignMessage(new string('-', 82)));
+				file.WriteLine(Display.alignMessage("ORIGEM"));
+				file.WriteLine(Display.alignMessage($"CONTA: {record.OriginClient.AccountNumber}"));
+				file.WriteLine(Display.alignMessage($"NOME: {record.OriginClient.Name}"));
+				file.WriteLine(Display.alignMessage($"CPF: {record.OriginClient.Cpf}"));
+			}
+			file.WriteLine(Display.alignMessage(new string('=', 82)));
+			file.Close();
 		}
 	}
 }
