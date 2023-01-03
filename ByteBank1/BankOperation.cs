68008b01aa29a57396fd4ff8e4ea1dcbd4f5e58d
brunosas88 <bruno.sas@live.com>
2023-01-03 16:53:54 -0300

    feat: add enum for better readability

---
+++ ByteBank1/BankOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank1
+{
+	public enum BankOperation
+	{
+		Deposit = 1,
+		Withdraw = 2,
+		Transfer = 3
+	};
+}
